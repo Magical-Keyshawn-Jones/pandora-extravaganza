c6263f7eece5471d4f10769aeefec6950083d8b2
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-12-02 12:36:26 -0600

    d

--- pandoraBackend/settings.py
+++ pandoraBackend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pandoraApp.apps.PandoraappConfig'
+    'pandoraApp.apps.PandoraappConfig',
     'corsheaders',
     'rest_framework',
 ]
