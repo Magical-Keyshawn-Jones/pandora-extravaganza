9da66f1edd96aa1d02b3d9d7f611b77fd63f536b
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-12-02 12:00:55 -0600

    testing deployment

--- pandoraBackend/settings.py
+++ pandoraBackend/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('pandoraKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'pandora-extravaganza.herokuapp.com/']
 CORS_ALLOW_ALL_ORIGINS = True
 
 
@@ -129,6 +129,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static')
 ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipoManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
