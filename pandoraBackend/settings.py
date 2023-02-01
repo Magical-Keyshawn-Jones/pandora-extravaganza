360b578000ecc5a22a7db716ee4b118ff1bfa63a
Keyshawn Jones <keyshawnjones29@gmail.com>
2023-01-31 21:29:11 -0600

    fixed vercel deployment

--- pandoraBackend/settings.py
+++ pandoraBackend/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pandoraApp',
-    # 'pandoraApp.apps.PandoraappConfig',
     'corsheaders',
     'rest_framework',
 ]
