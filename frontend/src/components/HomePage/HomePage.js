9cdcd25bc0c0060d07efdc51e22b488bd11dce8f
Keyshawn Jones <keyshawnjones29@gmail.com>
2023-02-10 02:47:57 -0600

    finished most of title transitions

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,9 +1,54 @@
 import './Homepage.css'
 
+// Create a tab with "The end is never the end is then end" => infinite scroll
 export default function Homepage() {
+
+    function everFactory(word) {
+        const letters = word.split('')
+        const spanLetters = letters.map((item, index) => {
+            return item = <span key={index} className='EverHeader'>{item}</span>
+        })  
+        
+        return spanLetters.map(item => {return item})
+    }
+
+    function fatabulousFactory(sentence) {
+        const wordArray = sentence.split(' ')
+        const spanWords = wordArray.map((item, index) => {
+            return item = <h4 key={index} className='fatabulousHeader'>{item} </h4>
+        })
+        return spanWords.map(item => {return item})
+    }
+
+    // const observer = new IntersectionObserver((entries) => {
+    //     entries.forEach((entry) => {
+    //         entry.target.classList.toggle('pink', entry.isIntersecting)
+    //         if(entry.isIntersecting) {
+    //             observer.unobserve(entry.target)
+    //         }
+    //     })
+    // }, {
+    //     threshold: 0
+    // })
+
+    // useEffect(()=>{
+    //     const elements = document.querySelectorAll('.Box')
+    //     elements.forEach((element => observer.observe(element)))
+    // },[])
+
     return (
         <main className='Homepage'>
-            <p>Welcome to the homepage!</p>
+            <header>
+                <div className='HomeHeaderTitle'>
+                    <div className='PandoraContainer'>
+                        <h1 className='PandoraTitle'>Pandora </h1> <h1 className='PandoraTitle'> Extravaganza!</h1>
+                    </div>
+                    <div className='FatabulousContainer'>
+                        {fatabulousFactory("So fatabulous you'll never want to leave!")}
+                    </div>
+                    <h4>{everFactory('...ever')}</h4>       
+                </div>
+            </header>
         </main>
     )
 }
